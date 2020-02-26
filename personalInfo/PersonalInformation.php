85069e869871c794ec4ca6ede237363b4b70a84c
Jacob Lapish <MightyMelonJL@gmail.com>
2020-02-26 13:16:54 -0500

    style sheet for personal info
    completed the first draft of the stylesheet for the personal info page

--- personalInfo/PersonalInformation.php
+++ personalInfo/PersonalInformation.php
@@ -1,6 +1,4 @@
 ﻿<!DOCTYPE html>
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Carleton Central | Personal Information</title>
 
@@ -11,7 +9,7 @@
      <link rel="shortcut icon" href="../media/carleton_mini_logo.png">
 
     <!-- Link to side stylesheet -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./PersonalInfo.css">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,66 +42,64 @@
 
         <div class="right"></div>
     </div>
-    <div>
-        <h1>Your Information</h1>
-    </div>
-    <div>
+    <div class="info">
         <div>
-            <h2>Your Name</h2>
-            <p>To change your legal name or perferred first name visit the registrars office (300 Tory Building - 8:30 to 4:30 Monday to Friday)</p>
-            <?php
-            echo "<h3>" . $_REQUEST["address"] . "</h3>";
-            ?>   
+            <h1>Your Information</h1>
+        </div>
+        <div>
+            <div>
+                <h2>Your Name</h2>
+                <p>To change your legal name or perferred first name visit the registrars office (300 Tory Building - 8:30 to 4:30 Monday to Friday)</p>
+                <?php
+                echo "<h3>" . $_REQUEST["address"] . "</h3>";
+                ?>   
+                <div>
+            <p>MyCarletonOne Account information and email</p>
+            <p>To change your MyCarletonOne Account information visit </p><a href="https://myone.carleton.ca">myone.carleton.ca</a>
+            </div>
+            <div>
+                <p>Your SIN number is on Record</p>
+                <p>To change your SIN number contact student_accounts@carleton.ca</p>
+            </div>
             <form action="EditPersonalInfo.html">
-                <input type="submit" value="Edit Address" />
-            </form>
+                    <input type="submit" value="Edit Address" />
+                </form>
+            <a href="#">Trevel Regestry</a>
             <div>
-        <p>MyCarletonOne Account information and email</p>
-        <p>To change your MyCarletonOne Account information visit </p><a href="https://myone.carleton.ca">myone.carleton.ca</a>
-        </div>
-        <div>
-            <p>Your SIN number is on Record</p>
-            <p>To change your SIN number contact student_accounts@carleton.ca</p>
-        </div>
-        <a href="#">Trevel Regestry</a>
-        <div>
-        <a href="https://wcc.carleton.ca/student/welcome.php">Campus Card</a>
-        </div>
+            <a href="https://wcc.carleton.ca/student/welcome.php">Campus Card</a>
+            </div>
 
-        </div>
-            <h1>Emergency Contact Information</h1>
-            <form action="EditInfo.html">
-                <input type="submit" value="Edit" />
-            </form>
-            <label for="contactName"><b>Name</b></label>
-            <?php
-            echo "<h3>" . $_REQUEST["contactName"] . "</h3>";
-            ?>
+            </div>
+                <h1>Emergency Contact Information</h1>
+                <form action="EditInfo.html">
+                    <input type="submit" value="Edit" />
+                </form>
+                <input type="text" for="contactName" readonly></input><br><br>
+                <?php
+                echo "<h3>" . $_REQUEST["contactName"] . "</h3>";
+                ?>
+                <div>
+                <input type="text" for="contactAddress" readonly></input><br><br>
+                    <?php
+                    echo "<h3>" . $_REQUEST["contactNumber"] . "</h3>";
+                    ?>
+                     <input type="text" for="contactAddress" readonly></input><br><br>
+                    <?php
+                    echo "<h3>" . $_REQUEST["contactAddress"] . "</h3>";
+                    ?>
+                    <?php
+                    echo "<h3>" . $_REQUEST["contactPostalCode"] . "</h3>";
+                    ?>
+                    <?php
+                    echo "<h3>" . $_REQUEST["contactCountry"] . "</h3>";
+                    ?>
+                     <input type="text" for="ContactRelationship" readonly></input><br><br>
+                    <?php
+                    echo "<h3>" . $_REQUEST["contactRelationship"] . "</h3>";
+                    ?>
+                </div>
             <div>
-            <label for="contactAddress"><b>Address and Phone</b></label>
-            <?php
-            echo "<h3>" . $_REQUEST["contactAddress"] . "</h3>";
-            ?>
-            <?php
-            echo "<h3>" . $_REQUEST["contactPostalCode"] . "</h3>";
-            ?>
-            <?php
-            echo "<h3>" . $_REQUEST["contactCountry"] . "</h3>";
-            ?>
-            <?php
-            echo "<h3>" . $_REQUEST["contactNumber"] . "</h3>";
-            ?>
-            </div>
-            <label for="contactRelationship"><b>Relationship</b></label>
-            <?php
-            echo "<h3>" . $_REQUEST["contactRelationship"] . "</h3>";
-            ?>
-            </div>
-        <div>
+        </div>
     </div>
-
-    <footer>
-        <h1>Temp Footer</h1>
-    </footer>
 </body>
 </html>
