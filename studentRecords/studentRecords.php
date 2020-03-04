334267d94b26522df219af10fde20833ef14c2d2
Jacob Lapish <MightyMelonJL@gmail.com>
2020-03-04 11:21:53 -0500

    css for student records complete
    finished the student records css and fixed a minor issue with the personal info css

--- studentRecords/studentRecords.php
+++ studentRecords/studentRecords.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="../media/carleton_mini_logo.png">
 
     <!-- Link to side stylesheet -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../studentRecords/studentrecords.css">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,41 +27,45 @@
         <h1>
             Student Records
         </h1>
+        <p>Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat</p>
 
         <!-- List of options -->
 
         <!-- Changing from a list to a set of divs because I think it will be easier to implement the CSS that way. Like give them each a class of "card" and id of "left", "middle", or "right" -->
+        <div id="wrapper">
+            <div class="support">
+                <img id="raven-logo" src="../media/support-service.png"/>
+                <h2>
+                    Support Services
+                </h2>
+                <p>
+                    Manage your academic and non-academic support services.
+                </p>
+                <button class="view" type="button">View</button>
+            </div>
 
-        <div>
-            <h2>
-                Support Services
-            </h2>
-            <p>
-                Manage your academic and non-academic support services.
-            </p>
-            <button type="button">View</button>
+            <div class="admissions">
+                <img id="raven-logo" src="../media/Admission.png"/>
+                <h2>
+                    Admissions
+                </h2>
+                <p>
+                    View and manage your applications and offers.
+                </p>
+                <button class="view" type="button">View</button>
+            </div>
+
+            <div class="grades">
+                <img id="raven-logo" src="../media/myGrades.png"/>
+                <h2>
+                    MyGrades
+                </h2>
+                <p>
+                    View your final grades for a selected term.
+                </p>
+                <button class="view" type="button">View</button>
+            </div>
         </div>
-
-        <div>
-            <h2>
-                Admissions
-            </h2>
-            <p>
-                View and manage your applications and offers.
-            </p>
-            <button type="button">View</button>
-        </div>
-
-        <div>
-            <h2>
-                MyGrades
-            </h2>
-            <p>
-                View your final grades for a selected term.
-            </p>
-            <button type="button">View</button>
-        </div>
-
     </div>
     <!-- End of Content -->
 
