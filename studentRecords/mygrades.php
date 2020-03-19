1b3051f31c6d38910f156d08a4ca9a9baa8fc482
Jacob Lapish <MightyMelonJL@gmail.com>
2020-03-19 17:19:28 -0400

    css for display grades

--- studentRecords/mygrades.php
+++ studentRecords/mygrades.php
@@ -1,6 +1,5 @@
-<?php 
-	require_once('../server/server.php');
-?>
+
+<!--add the server back -->
 
 <head>
     <title>Carleton Central | MyGrades</title>
@@ -12,7 +11,7 @@
     <link rel="shortcut icon" href="../media/carleton_mini_logo.png">
 
     <!-- Link to side stylesheet -->
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./myGrades.css">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,28 +19,72 @@
 
 <body>
     <!-- Navbar -->
-    <div class="navbar">
+    <div class="cu-navbar">
         <?php include '../templates/navbar.php' ?>
     </div>
     <!-- End of Navbar -->
 	
 	<!-- Content -->
-	<div>
-		<label for="semester">Select semester:</label>
-		<select id="semester" name="semseterList" form="semseterForm">
-			<option value="Fall 2019">Fall 2019</option>
-			<option value="Winter 2020">Winter 2020</option>
-		</select>
-		
-		<form method="POST" action="mygrades.php" id="semesterForm" name="viewSemester">
-			<input type="submit">
-		</form>
-		
-		<?php
-			if(isset($_POST['viewSemester'])) {
+
+	<div id="Wrapper">
+			<div class="program">
+				<h1 class="programTitle">Current Program</h1>
+				<p>Lorum Ipsum type beat</p><br>
+				<div class="linkBox">
+					<img
+							id="raven-logo"
+							src="../media/program-status.png"
+					/>
+					<h3>Program Status</h3>
+					<p>Lorum Ipsum type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat</p>                
+				</div>
+			</div>
+					<div class="info">
+						<div class="titleText">
+							<h1>View Grades</h1>
+							<p>Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat</p>
+						</div>
+					</div>
+
+		<div class="forms">
+			<div>
+				<select id="semester" name="semseterList" form="semseterForm">
+					<option value="0">Select a Semester</option>
+					<option value="Fall 2019">Fall 2019</option>
+					<option value="Winter 2020">Winter 2020</option>
+				</select>
 				
-			}
-		?>
+				<form method="POST" action="mygrades.php" id="semesterForm" name="viewSemester">
+					<input type="submit">
+				</form>
+				
+				<?php
+					if(isset($_POST['viewSemester'])) {
+						
+					}
+				?>
+			</div>
+
+			<div>
+				<div class="info">
+							<div class="titleText">
+								<h1>Request Audit</h1>
+								<p>Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat</p>
+							</div>
+						</div>
+
+				<select id="semester" name="semseterList" form="semseterForm">
+				<option value="0">Select a Semester</option>
+						<option value="Fall 2019">Fall 2019</option>
+						<option value="Winter 2020">Winter 2020</option>
+				</select>
+
+				<form method="POST" action="mygrades.php" id="semesterForm" name="viewSemester">
+						<input type="submit">
+					</form>
+			</div>
+		</div>
+
 	</div>
 	<!-- End of Content -->
 
