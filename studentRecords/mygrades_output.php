87e5e540685257600f58fe81ab4611fdb893f3d4
brandonwhiteimd <60040405+brandonwhiteimd@users.noreply.github.com>
2020-03-23 15:36:50 -0400

    Update mygrades_output.php
    + Added all the PHP to HOPEFULLY make this work

--- studentRecords/mygrades_output.php
+++ studentRecords/mygrades_output.php
@@ -42,12 +42,68 @@
 			</div>
 					<div class="info">
 						<div class="titleText">
-							<h1>View Grades</h1>
+							<h1>View Grades for <?php $_GET['term']; ?></h1>
 							<p>Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat Lorum ipsum dolar type beat</p>
 						</div>
 					</div>
 
 		<div>
+			<?php 
+				
+				// Set useful variables.
+				$term = $_GET['term'];
+				$courseListId = $_SESSION['coursesid'];
+				
+				// Now let's put the course IDs in one array and grades in another.
+				$a_courseIds = array();
+				$a_grades = array();
+				$numCourses = 0;
+				
+				$query = "SELECT * FROM ListOfCourses WHERE CourseList_ID = '$courseListId' AND Term = '$term'";
+				$result = $conn->query($query);
+				
+				if ($result->num_rows > 0) {		
+					
+					while ($row = $result->fetch_assoc()) {
+						
+						$a_courseIds[$numCourses] = $row['Course_ID'];
+						$a_grades[$numCourses] = $row['Grade'];
+						$numCourses++;
+					}
+				}
+				
+				// Cool, now lets get course names. 
+				$a_courseNames = array();
+				
+				for ($i=0; $i < $numCourses; $i++;) {
+					
+					$query = "SELECT CourseName FROM CourseInfo WHERE Course_ID = '$a_courseIds[$i]'"; 
+					$result = $conn->query($query);
+					
+					$row = $result->fetch_assoc();
+					
+					$a_courseNames[$i] = $row['CourseName'];
+				}
+				
+				// Finally we throw it all together in a table.
+				echo	"<table>";
+				echo		"<tr>";
+				echo			"<th>Course ID</th>";
+				echo			"<th>Course Name</th>";
+				echo			"<th>Final Grade</th>";
+				echo		"</tr>";
+					
+						
+				for ($i=0; $i < $numCourses; $i++;) {
+					echo 	"<tr>";
+					echo 		"<td>" . a_courseIds[$i] . "</td>";
+					echo 		"<td>" . a_courseNames[$i] . "</td>";
+					echo 		"<td>" . a_grades[$i] . "</td>";
+					echo 	"</tr>"
+				}
+				
+				echo	"</table>";		
+			?>
 		</div>
 
 	</div>
